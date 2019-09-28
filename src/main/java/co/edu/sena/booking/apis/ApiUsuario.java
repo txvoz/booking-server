f59884489ad91f3cc594abdce7feeccdcb76b527
luberf <luberf@live.com>
2019-09-27 21:26:38 -0500

    Se crea la ApiUsuario

---
+++ src/main/java/co/edu/sena/booking/apis/ApiUsuario.java
@@ -0,0 +1,162 @@
+package co.edu.sena.booking.apis;
+
+import co.edu.sena.booking.apis.abstract_.BasicApi;
+import co.edu.sena.booking.apis.abstract_.IApi;
+import co.edu.sena.booking.jpa.controllers.UsuarioJpaController;
+import co.edu.sena.booking.jpa.entities.Tipoidentificacion;
+import co.edu.sena.booking.jpa.entities.Usuario;
+import static co.edu.sena.booking.jpa.entities.Usuario_.fkTipoIdentificacion;
+import co.edu.sena.booking.utils.JsonTransformer;
+import co.edu.sena.booking.utils.Utils;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.util.Hashtable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import spark.Request;
+import spark.Response;
+
+public class ApiUsuario extends BasicApi implements IApi {
+
+    private static ApiUsuario instance = null;
+    private String path = "/usuario";
+    private Gson gson = null;
+    private UsuarioJpaController usuarioController = null;
+
+    private ApiUsuario() {
+        usuarioController = new UsuarioJpaController(Utils.getEM());
+        gson = JsonTransformer.singleton().getGson();
+        init();
+    }
+
+    public static ApiUsuario singleton() {
+        if (instance == null) {
+            instance = new ApiUsuario();
+        }
+        return instance;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public Object create(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            String body = rq.body();
+            Usuario nEntity = gson.fromJson(body, Usuario.class);
+            usuarioController.create(nEntity);
+               /* nEntity.setUsuId(nEntity.getUsuId());
+                nEntity.setUsuIdentificacion(nEntity.getUsuIdentificacion());
+                nEntity.setUsuNombres(nEntity.getUsuNombres());
+                nEntity.setUsuGenero(nEntity.getUsuGenero());
+                nEntity.setUsuCorreo(nEntity.getUsuCorreo());
+                nEntity.setUsuTelefono(nEntity.getUsuTelefono());
+                nEntity.setUsuAvatar(nEntity.getUsuAvatar());
+                nEntity.setFkTipoIdentificacion(nEntity.getFkTipoIdentificacion());*/
+            
+            retorno.put("status", 201);
+            retorno.put("message", "Registro creado con exito!");
+            retorno.put("data", nEntity);
+        } catch (JsonSyntaxException | PersistenceException e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object update(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            String body = rq.body();
+            Usuario nEntity = gson.fromJson(body, Usuario.class);
+            Usuario oEntity = usuarioController.findUsuario(id);
+            if (oEntity != null) {
+                oEntity.setUsuIdentificacion(nEntity.getUsuIdentificacion());
+                oEntity.setUsuNombres(nEntity.getUsuNombres());
+                oEntity.setUsuGenero(nEntity.getUsuGenero());
+                oEntity.setUsuCorreo(nEntity.getUsuCorreo());
+                oEntity.setUsuTelefono(nEntity.getUsuTelefono());
+                oEntity.setUsuAvatar(nEntity.getUsuAvatar());
+                usuarioController.edit(oEntity);
+                rs.status(201);
+                retorno.put("status", 201);
+                retorno.put("message", "Registro con el id@" + id + " actualizado!");
+                retorno.put("data", oEntity);
+            } else {
+                rs.status(404);
+                retorno.put("status", 404);
+                retorno.put("message", "Registro con el id@" + id + " no exite!");
+            }
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object delete(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            usuarioController.destroy(id);
+            retorno.put("status", 200);
+            retorno.put("message", "Registro eliminado con exito!");
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object getAll(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        List<Usuario> entities = usuarioController.findUsuarioEntities();
+        if (entities.size() > 0) {
+            retorno.put("status", 200);
+            retorno.put("message", "Datos encontrados con exito!");
+            retorno.put("data", entities);
+        } else {
+            rs.status(404);
+            retorno.put("status", 404);
+            retorno.put("message", "Datos no encontrados!");
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object getById(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            Usuario entity = usuarioController.findUsuario(id);
+            if (entity != null) {
+                retorno.put("status", 200);
+                retorno.put("message", "Registro con el id@" + id + " encontrado!");
+                retorno.put("data", entity);
+            } else {
+                rs.status(404);
+                retorno.put("status", 404);
+                retorno.put("message", "Registro con el id@" + id + " no exite!");
+            }
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+}
