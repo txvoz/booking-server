6f8360394c517b6e1024a33cfc83282d6b2a91e3
luberf <luberf@live.com>
2019-10-02 13:52:25 -0500

    Se modifica listarusuario .html y .js| Listar esta funcionando|falta crear modificar y eliminar

--- src/main/resources/public/web/appclient/js/listarUsuario.js
+++ src/main/resources/public/web/appclient/js/listarUsuario.js
@@ -1,21 +1,26 @@
-function detalle(target){
-    var id = $(target).data("usuId");
-    window.location.replace("?p=detalleUsuario&usuId="+id);
+function detalle(target)
+{
+    var id = $(target).data("id");
+    window.location.replace("?p=detalleUsuario&id="+id);
 }
 
-function eliminar(target) {
-    var id = $(target).data("usuId");
+function eliminar(target) 
+{
+    var id = $(target).data("id");
     httpConnect("/usuario/" + id, null, "DELETE",
-            function (r) {
+            function (r) 
+            {
                 alert(r.message);
                 cargarDatos();
             });
 }
-
-function cargarDatos() {
+    
+function cargarDatos() 
+{
     httpConnect("/usuario", null, "GET", function (r) {
         var html = "";
-        for (var i = 0; i < r.data.length; i++) {
+        for (var i = 0; i < r.data.length; i++) 
+        {
             var usuario = r.data[i];
             html += "<tr>";
             html += "<td>" + usuario.usuId + "</td>";
@@ -25,7 +30,6 @@ function cargarDatos() {
             html += "<td>" + usuario.usuCorreo + "</td>";
             html += "<td>" + usuario.usuTelefono + "</td>";
             html += "<td>" + usuario.usuAvatar + "</td>";
-            //html += "<td>" + usuario.fkTipoIdentificacion + "</td>";
             html += "<td>";
             html += "<div data-id='" + i + "' class='material-icons delete' style='color:red'>delete</div>";
             html += "<div data-id='" + i + "' class='material-icons edit' style='color:green'>edit</div>";
@@ -35,17 +39,20 @@ function cargarDatos() {
         $("tbody").html(html);
 
         $(".delete").click(function () {
-            if (confirm("Desea eliminar el recurso?")) {
+            if (confirm("Desea eliminar el recurso?")) 
+            {
                 eliminar(this);
             }
         });
-        $(".edit").click(function () {
+        $(".edit").click(function () 
+        {
             detalle(this);
         });
 
     });
 }
 
-$(function () {
+$(function () 
+{
     cargarDatos();
 });
