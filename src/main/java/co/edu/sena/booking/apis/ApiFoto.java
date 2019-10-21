acb0650b51debabbe12bad4929475002295bf133
zuluaga85 <54607231+zuluaga85@users.noreply.github.com>
2019-10-21 17:36:54 -0500

    Agrego ApiFoto.java

---
+++ src/main/java/co/edu/sena/booking/apis/ApiFoto.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.sena.booking.apis;
+
+import co.edu.sena.booking.apis.abstract_.BasicApi;
+import co.edu.sena.booking.apis.abstract_.IApi;
+import co.edu.sena.booking.jpa.controllers.FotoJpaController;
+import co.edu.sena.booking.jpa.entities.Foto;
+import co.edu.sena.booking.utils.Utils;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.util.Hashtable;
+import java.util.List;
+import javax.persistence.PersistenceException;
+import spark.Request;
+import spark.Response;
+
+/**
+ *
+ * @author gestion.ti
+ */
+public class ApiFoto extends BasicApi implements IApi{
+    
+    private static ApiFoto instance = null;
+    private String path = "/foto";
+    private Gson gson = null;
+    private FotoJpaController fotoController = null;
+    
+    private ApiFoto() {
+        fotoController = new FotoJpaController(Utils.getEM());
+        gson = new Gson();
+        init();
+    }
+    
+    public static ApiFoto singleton() {
+        if (instance == null) {
+            instance = new ApiFoto();
+        }
+        return instance;
+    }
+
+    @Override
+    public String getPath() {
+       return path;   
+    }
+
+    @Override
+    public Object create(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            String body = rq.body();
+            Foto nEntity = gson.fromJson(body, Foto.class);
+            fotoController.create(nEntity);
+            retorno.put("status", 201);
+            retorno.put("message", "Registro creado con exito!");
+            retorno.put("data", nEntity);
+        } catch (JsonSyntaxException | PersistenceException e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object update(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            String body = rq.body();
+            Foto nEntity = gson.fromJson(body, Foto.class);
+            Foto oEntity = fotoController.findFoto(id);
+            if (oEntity != null) {
+                oEntity.setFotRuta(nEntity.getFotRuta());
+                oEntity.setFotLabel(nEntity.getFotLabel());
+                oEntity.setFotDescripcion(nEntity.getFotDescripcion());
+                fotoController.edit(oEntity);
+                rs.status(201);
+                retorno.put("status", 201);
+                retorno.put("message", "Registro con el id@" + id + " actualizado!");
+                retorno.put("data", oEntity);
+            } else {
+                rs.status(404);
+                retorno.put("status", 404);
+                retorno.put("message", "Registro con el id@" + id + " no exite!");
+            }
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object delete(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            fotoController.destroy(id);
+            retorno.put("status", 200);
+            retorno.put("message", "Registro eliminado con exito!");
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object getAll(Request rq, Response rs) {
+        Hashtable<String, Object> retorno = new Hashtable<>();
+        List<Foto> entities = fotoController.findFotoEntities();
+        if (entities.size() > 0) {
+            retorno.put("status", 200);
+            retorno.put("message", "Datos encontrados con exito!");
+            retorno.put("data", entities);
+        } else {
+            rs.status(404);
+            retorno.put("status", 404);
+            retorno.put("message", "Datos no encontrados!");
+        }
+        return retorno;
+    }
+
+    @Override
+    public Object getById(Request rq, Response rs) {
+         Hashtable<String, Object> retorno = new Hashtable<>();
+        try {
+            int id = Integer.parseInt(rq.params("id"));
+            Foto entity = fotoController.findFoto(id);
+            if (entity != null) {
+                retorno.put("status", 200);
+                retorno.put("message", "Registro con el id@" + id + " encontrado!");
+                retorno.put("data", entity);
+            } else {
+                rs.status(404);
+                retorno.put("status", 404);
+                retorno.put("message", "Registro con el id@" + id + " no exite!");
+            }
+        } catch (Exception e) {
+            rs.status(400);
+            retorno.put("status", 400);
+            retorno.put("message", e.getMessage());
+        }
+        return retorno;
+    }
+    
+}
