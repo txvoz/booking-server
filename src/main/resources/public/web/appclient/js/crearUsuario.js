1d08b120af0b7fcea05dc593277c44710b2b5def
GUSTAVO ADOLFO RODRIGUEZ QUINAYAS <garodriguez335@misena.edu.co>
2019-10-21 19:11:31 -0500

    -Ajustes

--- src/main/resources/public/web/appclient/js/crearUsuario.js
+++ src/main/resources/public/web/appclient/js/crearUsuario.js
@@ -1,6 +1,13 @@
-$(function(){
-    cargarCategorias();
-    $("#frmCrear").submit(function(){
+function cargarTipoIdentificacion() {
+    httpConnect("/usuario", jusuario, "POST", function (r) {
+        alert(r.message + "-" + r.data.usuIdentificacion);
+        $("button[type=reset]").click();
+    });
+}
+
+$(function () {
+    cargarTipoIdentificion();
+    $("#frmCrear").submit(function () {
         var usuario = new Object();
         usuario.usuIdentificacion = $("#usuIdentificacion").val();
         usuario.usuNombres = $("#usuNombres").val();
@@ -9,14 +16,14 @@ $(function(){
         usuario.usuTelefono = $("#usuTelefono").val();
         usuario.usuAvatar = $("#usuAvatar").val();
         usuario.FkTipoIdentificacion = $("#FkTipoIdentificacion").val();
-        
+
         var jusuario = JSON.stringify(usuario);
-        
-        httpConnect("/usuario",jusuario,"POST",function(r){
-            alert(r.message+"-"+r.data.usuIdentificacion);
+
+        httpConnect("/usuario", jusuario, "POST", function (r) {
+            alert(r.message + "-" + r.data.usuIdentificacion);
             $("button[type=reset]").click();
         });
-        
+
         return false;
     });
 });
